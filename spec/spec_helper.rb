1d058bcbdfab6453c60f28c011befef11db43513
jonah honeyman <jonah@honeyman.org>
2013-07-26 15:59:33 +0300

    choose non-default port for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,9 @@ require File.expand_path("../../lib/komodor", __FILE__)
 RSpec.configure do |conf|
   conf.before(:suite) do
     require 'logger'
-    Komodor.config do |conf|
-      conf.logger = Logger.new("/tmp/komodor.test.log")
+    Komodor.config do |config|
+      config.logger = Logger.new("/tmp/komodor.test.log")
+      config.port = 60002
     end
   end
 end
